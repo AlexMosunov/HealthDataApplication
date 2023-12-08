04c27abd38fad1cc387cdd74ba6e0ace5f19234f
Oleksii Mosunov <o.mosunov@mobidev.biz>
2023-12-08 16:49:38 +0100

    Initial Commit

---
+++ HealthDataApplication/HealthDataApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDataApplicationApp.swift
+//  HealthDataApplication
+//
+//  Created by User on 08.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthDataApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
